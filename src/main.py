c9c3bffc7063100052dca82531a6363765f1f5d0
Jakob Van Alstine <vanalstinejakob@gmail.com>
2026-02-18 16:18:46 +0100

    init: toy static site generator

---
+++ src/main.py
@@ -0,0 +1,12 @@
+from textnode import TextType, TextNode
+
+
+def main():
+    dummy = TextNode(
+        "This is some anchor text", TextType.LINK, "https://www.boot.dev"
+    )
+    print(repr(dummy))
+
+
+if __name__ == "__main__":
+    main()
